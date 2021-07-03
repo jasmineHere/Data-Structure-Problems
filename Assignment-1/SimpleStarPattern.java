d6cc8be20fe859b6d21e0a4800e18a0dd690614a
Jasmine Muman <jasminemuman21@gmail.com>
2021-07-03 17:08:49 +0530

    Q7(a)

---
+++ Assignment-1/SimpleStarPattern.java
@@ -0,0 +1,20 @@
+public class SimpleStarPattern {
+
+    public static void pattern(int n) {
+        int id = 1, nst = 1 ;    
+        while (id <= n) {
+            int cst = 1;
+            while (cst <= nst) {
+                System.out.print("* ");
+                cst++;
+            }
+            System.out.println();
+            nst++;
+            id++;
+        }
+    }
+    
+    public static void main(String[] args) {
+        pattern(5);
+    }
+}
