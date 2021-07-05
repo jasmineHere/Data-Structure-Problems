32431c4eb8b0479aedd37f3c7f4124d814c189a7
Jasmine Muman <jasminemuman21@gmail.com>
2021-07-05 18:54:35 +0530

    Q3

--- Assignment-1/GCDProb.java
+++ Assignment-1/GCDProb.java
@@ -3,17 +3,23 @@ import java.util.*;
 public class GCDProb {
 
     public static void calculateGCD(int a, int b) {
-        for (int i = Math.min(a, b); i >= 1; i--) {
-            if (a % i == 0 && b % i == 0) {
-                System.out.println("Calculated GCD: " + i);
+        int GCD = 0;
+        if (a == 0 || b == 0) {
+            GCD = Math.max(a, b);
+        } else {
+            for (int i = Math.min(a, b); i > 1; i--) {
+                if (a % i == 0 && b % i == 0) {
+                    GCD = i;
+                }
             }
         }
+        System.out.println("Calculated GCD: " + GCD);
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a, b;
-        System.out.println("Enter two numbers to find their GCD: ");
+        System.out.print("Enter two numbers to find their GCD: ");
         a = sc.nextInt();
         b = sc.nextInt();
         calculateGCD(a, b);
